65e4eab2f75c98809bd3d8b118175693c771dc89
Victor Aguiar <112593695+victorNascimentoAguiar@users.noreply.github.com>
2024-01-18 16:36:21 -0300

    primeiro commit controle de hora

---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("controle de horas");
---
+++ models/ControleHora.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_De_Hora.models
+{
+    public class ControleHora
+    {
+        
+    }
+}
