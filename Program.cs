f2b3f5fd8ded39aca58977f2ee005a15b231e365
Victor Aguiar <112593695+victorNascimentoAguiar@users.noreply.github.com>
2024-01-18 19:23:59 -0300

    adicionando comentarios no código

--- Program.cs
+++ Program.cs
@@ -4,26 +4,30 @@ List<string>agenda = new List<string>();
 Console.WriteLine("sua agenda ");
 while(exibirMenu)
 {
+    //menu com as opçôes de função
     Console.WriteLine("1-adicionar  item na agenda");
     Console.WriteLine("2-remover item da agenda");
     Console.WriteLine("3-mostrar agenda");
     Console.WriteLine("4-encerrar programa");
 
-    opcao = Console.ReadLine();
+    opcao = Console.ReadLine();//obtendo a informação dada pelo usuario 
     switch(opcao)
     {
         case "1":
+        //adicionando item na lista agenda
             Console.WriteLine("adicionando item!");
             string input = Console.ReadLine();
             agenda.Add(input);
+            //tratamento de exeções caso o valor seja nulo "   "
             if (string.IsNullOrWhiteSpace(input))
             Console.WriteLine("item invalido");
             break;
 
         case "2":
+        //removendo item da lista agenda
             Console.WriteLine("removendo item da agenda!!");
             string remover = Console.ReadLine();
-            
+            //tratamento de exeções caso o item não esteja na agenda
             if(agenda.Contains(remover))
             {
                 agenda.Remove(remover);
@@ -36,6 +40,7 @@ while(exibirMenu)
             break;
 
         case "3":
+        //foreach que vai apresentar a lista agenda
             Console.WriteLine("abrindo agenda!!!");
             foreach(string item in agenda)
             {
@@ -44,11 +49,13 @@ while(exibirMenu)
             break;
 
         case "4":
+        //encerrando o programa para que o loop while não fique infinito
             Console.WriteLine("encerrando o programa.......");
             exibirMenu = false;
             break;
 
         default:
+        //tratamento de exeção caso o usuario digite uma opção que não existe
             Console.WriteLine("comando invalido");
             break;
 
